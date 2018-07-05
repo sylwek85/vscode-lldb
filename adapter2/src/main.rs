5b52665db8971c7ea51c093a7f910f1e3dd06fe6
Vadim Chugunov <vadimcn@gmail.com>
2018-07-05 11:31:21 -0700

    frame codec

--- adapter2/src/main.rs
+++ adapter2/src/main.rs
@@ -14,33 +14,56 @@ extern crate my_lldb as lldb;
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate tokio;
+extern crate tokio_io;
+extern crate bytes;
 
-use std::io;
-use std::net;
 use std::thread;
 use std::time::Duration;
 
+use tokio::io;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+
 mod debug_session;
-mod wire_protocol;
 mod must_initialize;
+mod wire_protocol;
 mod worker_thread;
 
 fn main() {
     env_logger::init();
-
-    let listener = net::TcpListener::bind("127.0.0.1:4711").unwrap();
-    let addr = listener.local_addr().unwrap();
+    let addr = "127.0.0.1:4711".parse().unwrap();
+    let listener = TcpListener::bind(&addr).unwrap();
     println!("Listening on port {}", addr.port());
-    let (conn, addr) = listener.accept().unwrap();
-    conn.set_nodelay(true);
-    let conn_write = conn.try_clone().unwrap();
 
-    let (debug_server, recv_message, send_message) =
-        wire_protocol::DebugServer::new(Box::new(io::BufReader::new(conn)), Box::new(conn_write));
+    let server = listener
+        .incoming()
+        .for_each(|conn| {
+            conn.set_nodelay(true);
+            let framed = conn.framed(wire_protocol::Codec::new());
 
-    let mut session = debug_session::DebugSession::new(send_message);
-    loop {
-        let message = recv_message.recv().unwrap();
-        session.handle_message(message);
-    }
+            let mut session = debug_session::DebugSession::new(send_message);
+
+            Ok(())
+        })
+        .map_err(|err| {
+            error!("accept error: {:?}", err);
+        });
+
+    tokio::run(server);
+
+    // let listener = net::TcpListener::bind("127.0.0.1:4711").unwrap();
+    // let addr = listener.local_addr().unwrap();
+    // let (conn, addr) = listener.accept().unwrap();
+    // conn.set_nodelay(true);
+    // let conn_write = conn.try_clone().unwrap();
+
+    // let (debug_server, recv_message, send_message) =
+    //     wire_protocol::DebugServer::new(Box::new(io::BufReader::new(conn)), Box::new(conn_write));
+
+    // let mut session = debug_session::DebugSession::new(send_message);
+    // loop {
+    //     let message = recv_message.recv().unwrap();
+    //     session.handle_message(message);
+    // }
 }
--- adapter2/src/wire_protocol.rs
+++ adapter2/src/wire_protocol.rs
@@ -1,65 +1,86 @@
+use bytes::{BufMut, BytesMut};
+use std::error::Error;
+use std::fmt::Write;
+use std::str;
+use tokio::io;
+use tokio_io::codec;
+
 use debug_protocol::ProtocolMessage;
 use serde_json::{self, Value};
-use std::error::Error;
-use std::io;
-use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
-use std::thread;
-use std::str;
 
-pub struct DebugServer {
-    sender: thread::JoinHandle<()>,
-    receiver: thread::JoinHandle<()>,
+enum State {
+    ReadingHeaders,
+    ReadingBody,
 }
 
-impl DebugServer {
-    pub fn new(
-        mut reader: Box<io::BufRead + Send>, mut writer: Box<io::Write + Send>,
-    ) -> (Self, Receiver<ProtocolMessage>, SyncSender<ProtocolMessage>) {
-        let (inbound_send, inbound_recv) = sync_channel::<ProtocolMessage>(100);
-        let (outbound_send, outbound_recv) = sync_channel::<ProtocolMessage>(100);
-        let outbound_send2 = outbound_send.clone();
+pub struct Codec {
+    state: State,
+    content_len: usize,
+}
 
-        let receiver = thread::spawn(move || {
-            let mut buffer: Vec<u8> = vec![];
-            let mut line = String::new();
-            loop {
-                line.clear();
-                reader.read_line(&mut line);
-                if line.starts_with("Content-Length:") {
-                    let content_len = line[15..].trim().parse::<usize>().unwrap();
-                    line.clear();
-                    reader.read_line(&mut line);
-                    buffer.resize(content_len, 0);
-                    reader.read_exact(&mut buffer).unwrap();
-                    debug!("rx: {}", str::from_utf8(&buffer).unwrap());
-                    match serde_json::from_slice(&buffer) {
-                        Ok(message) => {
-                            inbound_send.send(message);
+impl Codec {
+    pub fn new() -> Codec {
+        Codec {
+            state: State::ReadingHeaders,
+            content_len: 0,
+        }
+    }
+}
+
+impl codec::Decoder for Codec {
+    type Item = ProtocolMessage;
+    type Error = io::Error;
+
+    fn decode(&mut self, buffer: &mut BytesMut) -> Result<Option<ProtocolMessage>, io::Error> {
+        match self.state {
+            State::ReadingHeaders => {
+                if let Some(pos) = buffer.windows(2).position(|b| b == &[b'\r', b'\n']) {
+                    let line = buffer.split_to(pos + 2);
+                    if line.len() == 2 {
+                        self.state = State::ReadingBody;
+                    } else if let Ok(line) = str::from_utf8(&line) {
+                        if line.len() > 15 && line[..15].eq_ignore_ascii_case("content-length:") {
+                            if let Ok(content_len) = line[15..].trim().parse::<usize>() {
+                                self.content_len = content_len;
+                            }
                         }
+                    }
+                }
+            }
+            State::ReadingBody => {
+                if (buffer.len() >= self.content_len) {
+                    let message_bytes = buffer.split_to(self.content_len);
+                    self.state = State::ReadingHeaders;
+                    self.content_len = 0;
+
+                    debug!("rx: {}", str::from_utf8(&message_bytes).unwrap());
+                    match serde_json::from_slice(&message_bytes) {
+                        Ok(message) => return Ok(Some(message)),
                         Err(err) => {
                             if (err.is_data()) {
                                 // Try reading as generic JSON value.
-                                if let Ok(msg) = serde_json::from_slice::<Value>(&buffer) {
-                                    inbound_send.send(ProtocolMessage::Unknown(msg));
+                                if let Ok(message) = serde_json::from_slice::<Value>(&message_bytes) {
+                                    return Ok(Some(ProtocolMessage::Unknown(message)));
                                 }
                             }
                         }
                     }
                 }
             }
-        });
+        }
+        Ok(None)
+    }
+}
 
-        let sender = thread::spawn(move || loop {
-            let message = outbound_recv.recv().unwrap();
-            let buffer = serde_json::to_vec(&message).unwrap();
-            debug!("tx: {}", str::from_utf8(&buffer).unwrap());
-            write!(&mut writer, "Content-Length: {}\r\n\r\n", buffer.len());
-            writer.write_all(&buffer);
-            writer.flush();
-        });
+impl codec::Encoder for Codec {
+    type Item = ProtocolMessage;
+    type Error = io::Error;
 
-        let debug_server = DebugServer { sender, receiver };
-
-        (debug_server, inbound_recv, outbound_send)
+    fn encode(&mut self, message: ProtocolMessage, buffer: &mut BytesMut) -> Result<(), io::Error> {
+        let message_bytes = serde_json::to_vec(&message).unwrap();
+        debug!("tx: {}", str::from_utf8(&message_bytes).unwrap());
+        write!(buffer, "Content-Length: {}\r\n\r\n", message_bytes.len());
+        buffer.extend_from_slice(&message_bytes);
+        Ok(())
     }
 }
