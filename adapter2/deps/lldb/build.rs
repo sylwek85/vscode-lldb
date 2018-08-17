38d02b295e25b15ea31a7da50c2f753e42ca9539
Vadim Chugunov <vadimcn@gmail.com>
2018-08-16 18:29:05 -0700

    Use private headers

--- adapter2/deps/lldb/build.rs
+++ adapter2/deps/lldb/build.rs
@@ -2,7 +2,7 @@ extern crate cpp_build;
 
 fn main() {
     cpp_build::Config::new()
-        .include("/usr/lib/llvm-6.0/include")
+        .include("include")
         .build("src/lldb.rs");
     println!("cargo:rustc-link-search={}", "/usr/lib/llvm-6.0/lib");
     println!("cargo:rustc-link-lib={}", "lldb-6.0");
